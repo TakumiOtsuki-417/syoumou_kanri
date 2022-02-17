b324420a1b3b6adccf7cce51cfc24625d65fafc5
TakumiOtsuki-417 <heitaku443@gmail.com>
2022-02-18 01:22:06 +0900

    タブ切り替え
    週ごと、月ごとの切り替え実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,16 +26,19 @@ class ItemsController < ApplicationController
   end
   def search
     @items = Item.all
+    @damages = Damage.all
     # showのリストとの内容分岐
     @rest = nil
     if params["date(1i)"].to_i == nil
-      @search_items = "期間を選択してください"
+      @search_items = ""
+      @search_damages = ""
     else
       search_date = Date.new(params["search(1i)"].to_i, params["search(2i)"].to_i, params["search(3i)"].to_i)
       # search_year = params[:date].year
       # search_month = params[:date].month
       search_date_end = search_date.end_of_month
       @search_items = @items.where(date: search_date..search_date_end)
+      @search_damages = @damage.while(date: search_date..search_date_end)
     end
     # render :index
   end
--- app/javascript/search.js
+++
@@ -1,3 +0,0 @@
-    console.log("成功してる？");
-  $("#search_result").html("<%= j(render partial: 'shared/search', locals: {item: item, rest: @rest}) %>");
-  $('#task-form').html("<%= j (render 'shared/search') %>");
