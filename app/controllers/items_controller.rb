08f1fde0d29a0162d32b45e141ab92bf9d58d6a5
TakumiOtsuki-417 <heitaku443@gmail.com>
2022-02-18 20:30:04 +0900

    itemループリファクタリング
    ファイル数を削減

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,20 +36,6 @@ class ItemsController < ApplicationController
       @search_damages = @damages.where(date: search_date..search_date_end)
     end
   end
-  def search_d
-    @items = Item.all
-    @damages = Damage.all
-    # showのリストとの内容分岐
-    @rest = nil
-    if params["date(1i)"].to_i == nil
-      @search_items = "???"
-      @search_damages = "???"
-    else
-      search_date = Date.new(params["search(1i)"].to_i, params["search(2i)"].to_i, params["search(3i)"].to_i)
-      search_date_end = search_date.end_of_month
-      @search_damages = @damages.where(date: search_date..search_date_end)
-    end
-  end
   def show
     @item = Item.find(params[:id])
     @damages = Damage.where(item_id: @item.id)
